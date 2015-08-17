18ca1b0e154b1913d46c6bff4c5159c3d95475df
johnptmcdonald <johnptmcdonald@gmail.com>
2015-08-17 16:52:35 -0700

    adds TTT example code

---
+++ work/w02/d01/instructor/TTT_example/app2.js
@@ -0,0 +1,17 @@
+// 2. The boxes should respond to a click by doing something (in this case, run the takeTurn function:
+
+
+var boxes = document.querySelectorAll(".box");
+
+console.log(boxes);
+
+for(var i = 0; i < boxes.length; i++){
+	boxes[i].addEventListener("click", takeTurn)
+}
+
+function takeTurn(){
+	console.log(this);
+	this.innerHTML = "new content!";
+}
+
+
