42add1ee45eaa77939a1411620ee40d0298999e6
johnptmcdonald <johnptmcdonald@gmail.com>
2015-08-21 09:54:30 -0700

    adds code challenge answers

--- work/w02/d05/code_challenges/02_fizz_buzz.js
+++ work/w02/d05/code_challenges/02_fizz_buzz.js
@@ -5,3 +5,14 @@
 // If a number is divisible by both 3 AND 5, console.log "FizzBuzz".
 
 
+for(var i = 1; i <= 100; i++){
+	if(i%15 === 0){
+		console.log("fizzbuzz");
+	} else if(i%5 === 0){
+		console.log("buzz");
+	} else if(i%3 === 0){
+		console.log("fizz");
+	} else {
+		console.log(i);
+	}
+}
