ef2fa52e6efb756f8440455316b82e3713f2d3da
Eric Hodonsky <relic.melex@gmail.com>
2015-09-23 17:07:47 -0700

    Removes bad solution code, and extra lines in starter code

--- work/w07/d03/node-express-mongo-mongoose-models/lab/starter-code/app.js
+++ work/w07/d03/node-express-mongo-mongoose-models/lab/starter-code/app.js
@@ -5,7 +5,6 @@ var
   logger = require( "morgan" ),
   mongoose = require( "mongoose" ),
   bodyParser = require( "body-parser" ),
-  expressLayouts = require( "express-ejs-layouts"),
   app = express(),
   router = express.Router();
 
@@ -15,9 +14,6 @@ moongoose.connect( "mongodb://localhost/animalshelter" );
 app.use( logger( "dev" ) );
 app.use( bodyParser.urlencoded({ extended: true }) );
 app.set( "views", path.join( __dirname, "views" ) );
-app.use( expressLayouts );
-app.engine( "ejs", require( "ejs" ).renderFile );
-app.set( "view engine", "ejs" );
 
 // development error handler
 // will print stacktrace
