21ae19af30d406117896f618fcef032b6ff03228
Eric Hodonsky <relic.melex@gmail.com>
2015-09-24 19:23:09 -0700

    Removes views from lab

--- work/w07/d04/homework/express-routes-mongo-lab/solution-code/app.js
+++ work/w07/d04/homework/express-routes-mongo-lab/solution-code/app.js
@@ -13,13 +13,6 @@ app.use( logger( "dev" ) );
 app.use( bodyParser.json() );
 app.use( bodyParser.urlencoded( { extended: true } ) );
 
-app.set( "views", path.join( __dirname, "views" ) );
-app.engine( "ejs", require( "ejs" ).renderFile );
-app.set( "view engine", "ejs" );
-
-// Add static middleware
-app.use( express.static( __dirname + "/public" ) );
-
 
 app.use( routes );
 
