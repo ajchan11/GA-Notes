a8a208898ba9476dd6c49b1dd656ec06cb401b95
Eric Hodonsky <relic.melex@gmail.com>
2015-09-27 14:12:40 -0700

    Adds node express auth lesson and lab

---
+++ work/w08/d02/node-express-auth-lesson/starter-code/controllers/users.js
@@ -0,0 +1,29 @@
+var passport = require( "passport" );
+
+
+module.exports = {
+  // GET /signup
+  getSignup: function ( request, response ) {
+  
+  },
+  // POST /signup
+  postSignup: function ( request, response ) {
+
+  },
+  // GET /login
+  getLogin: function ( request, response ) { 
+
+  },
+  // POST /login 
+  postLogin: function ( request, response ) {
+
+  },
+  // GET /logout
+  getLogout: function ( request, response ) {
+  
+  },
+  // Restricted page
+  secret: function ( request, response ) {
+  
+  }
+}
